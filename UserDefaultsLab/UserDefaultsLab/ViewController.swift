0d08ef5b64e24cfed7e7110f4bd66e2d14456d90
uwagboe <sm.uwagboe@gmail.com>
2020-01-13 17:14:17 -0500

    the default value is set for the picker

--- UserDefaultsLab/UserDefaultsLab/ViewController.swift
+++ UserDefaultsLab/UserDefaultsLab/ViewController.swift
@@ -16,40 +16,44 @@ class ViewController: UIViewController {
     @IBOutlet weak var horoscopePicker: UIPickerView!
     
     var picker = [String]()
+    var defaultHoroscope = "Virgo"
     
     var selectedPicker = Horoscope.virgo {
         didSet {
+            
+            
+            
             UserSettings.shared.updateHoroscope(with: selectedPicker)
         }
         
     }
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
-         picker = ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"]
+        picker = ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"]
         
         horoscopePicker.delegate = self
         horoscopePicker.dataSource = self
-        
+       // setDefaultValue(item: Horoscope.virgo.rawValue)
+        horoscopePicker.selectRow(6, inComponent: 0, animated: true)
     }
     
     func updateUI(){
-        
         if let endingHoroscope = UserSettings.shared.whichHoroscope() {
             selectedPicker = endingHoroscope
         }
-        
     }
-   
+    
 
 }
 
 extension ViewController: UIPickerViewDelegate {
+    
 }
 
 extension ViewController: UIPickerViewDataSource {
-    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
